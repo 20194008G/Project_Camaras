390f229bee717eb905f96e56a4cdbf85326828fb
Gabriel Cristobal Gonzales <79822686+20194008G@users.noreply.github.com>
2024-09-25 22:05:20 -0500

    Update nodo_pub_abrir.py

--- my_pkg_py/my_pkg_py/nodo_pub_abrir.py
+++ my_pkg_py/my_pkg_py/nodo_pub_abrir.py
@@ -1,24 +1,24 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Bool
-import time
+
 class NodoPublicador(Node):
     def __init__(self):
         super().__init__("nodo_pub")
         self.publisher_ = self.create_publisher(Bool, "topico_1", 10)
+        self.timer = self.create_timer(1, self.timer_callback)  # Publica cada 1 segundo
         self.get_logger().info("Nodo publicador iniciado")
+
+    def timer_callback(self):
         msg = Bool()
-        msg.data=True ##Codigo abrir puerta       
+        msg.data = True  # Código para abrir puerta
         self.publisher_.publish(msg)
         self.get_logger().info(f'Publicando: "{msg.data}" para abrir la puerta')
-        time.sleep(5)
-        msg.data=False    
-        self.publisher_.publish(msg)
-        self.get_logger().info(f'Publicando: "{msg.data}" para abrir la puerta')
+
 def main(args=None):
     rclpy.init(args=args)
     nodo_publicador = NodoPublicador()
-    rclpy.spin(nodo_publicador)
+    rclpy.spin(nodo_publicador)  # Mantiene el nodo en funcionamiento hasta que lo detengas
     nodo_publicador.destroy_node()
     rclpy.shutdown()
 
